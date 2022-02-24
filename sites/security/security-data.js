203c1e007161e318715a2100a7466c09de558964
Boon <tayboonsiang90@gmail.com>
2022-02-24 09:31:09 +0800

    added to security.html

--- sites/security/security-data.js
+++ sites/security/security-data.js
@@ -1,20 +1,6 @@
+function hashOnce(message, hashFunction) {
+    hash = CryptoJS[hashFunction](message);
+    ciphertext = hash.toString(CryptoJS.enc.Hex);
 
-
-
-
-
-
-// //Pass in a hexdecimal string of any length and it will return the base58 encoding
-// function hexToBase58(hex) {
-//     const BASE58_CHARS = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"; //Bitcoin encoding (unique)
-//     let base10 = BigInt("0x" + hex); //BigInt usage to avoid truncation errors
-//     console.log("base10: ", base10);
-//     let result = "";
-//     while (base10 != 0) {
-//         //base conversion algorithm
-//         remainder = Number(base10 % 58n);
-//         result = BASE58_CHARS[remainder] + result;
-//         base10 = base10 / 58n;
-//     }
-//     return result;
-// }
+    return ciphertext;
+}
--- sites/security/security-display.js
+++ sites/security/security-display.js
@@ -1,9 +1,52 @@
 document.querySelector("#hash-example-1-plaintext").addEventListener("input", async function () {
     hashPicked = document.querySelector("#hash-example-1-select :checked").value;
-    messagePicked = document.querySelector("#hash-example-1-plaintext").value;
+    if (!hashPicked) {
+        document.querySelector("#hash-example-1-plaintext").value = "You did not pick a hash function. Please pick a hash!";
+    } else {
+        messagePicked = document.querySelector("#hash-example-1-plaintext").value;
 
-    hash = CryptoJS[hashPicked](messagePicked);
-    ciphertext = hash.toString(CryptoJS.enc.Hex);
-    document.querySelector("#hash-example-1-message").innerText = `ciphertext = ${hashPicked} ( plaintext )`;
-    document.querySelector("#hash-example-1-ciphertext").value = ciphertext;
+        ciphertext = hashOnce(messagePicked, hashPicked);
+
+        document.querySelector("#hash-example-1-message").innerText = `ciphertext = ${hashPicked} ( plaintext )`;
+        document.querySelector("#hash-example-1-ciphertext").value = ciphertext;
+    }
+});
+
+document.querySelector("#hash-example-1-select").addEventListener("change", function () {
+    console.log("triggered");
+    document.querySelector("#hash-example-1-plaintext").value = `Please key in your message to be hashed.`;
+});
+
+document.querySelector("#hash-example-2-button").addEventListener("click", function () {
+    marker = true;
+    const genRanHex = (size) => [...Array(size)].map(() => Math.floor(Math.random() * 16).toString(16)).join("");
+    hashPicked = document.querySelector("#hash-example-2-select :checked").value;
+    ciphertextArray = [];
+    count = 0;
+    for (i = 0; i < 10; i++) {
+        console.log(count);
+        plaintext = genRanHex(128);
+        ciphertext = hashOnce(plaintext, hashPicked);
+        collisionFlag = ciphertextArray.includes(ciphertext);
+        if (collisionFlag) {
+            break;
+        } else {
+            ciphertextArray.push(ciphertext);
+        }
+        count++;
+        tr = document.createElement("tr");
+        td1 = document.createElement("td");
+        td1.innerText = count;
+        td2 = document.createElement("td");
+        td2.innerText = plaintext;
+        td3 = document.createElement("td");
+        td3.innerText = ciphertext;
+        td4 = document.createElement("td");
+        td4.innerText = collisionFlag;
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+        tr.appendChild(td4);
+        document.querySelector("#hash-output").appendChild(tr);
+    }
 });
