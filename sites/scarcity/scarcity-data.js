d3f0a7061b8f2d9f3576a25e75f16742d58f59be
Boon <tayboonsiang90@gmail.com>
2022-03-03 00:14:01 +0800

    Added to scarcity.html

---
+++ sites/scarcity/scarcity-data.js
@@ -0,0 +1,21 @@
+function bitcoinSupplyData() {
+    const BLOCKTIME = 10; //in Minutes
+    const EPOCHBLOCKS = 210000; //Blocks every halving
+    let currDate = new Date("2009-01-04 02:15"); //date and time of first block
+    let noOfBlocksFortnight = (24 * 60 * 14) / BLOCKTIME; //Total number of minutes in a day divided by block time
+    let fortnightsPerEpoch = EPOCHBLOCKS / noOfBlocksFortnight; //Number of fortnights in an Epoch
+    let blockReward = 50; //Initial block reward
+    let currentSupply = 0; //Initial bitcoinSupplyData
+
+    let dataArray = [];
+    for (let i = 0; i < 12; i++) {
+        for (let j = 1; j < fortnightsPerEpoch; j++) {
+            currDate.setDate(currDate.getDate() + 14);
+            currentSupply += blockReward * noOfBlocksFortnight;
+            dataArray.push([new Date(currDate), currentSupply]);
+        }
+        blockReward = blockReward / 2;
+    }
+
+    return dataArray;
+}
---
+++ sites/scarcity/scarcity-display.js
@@ -0,0 +1,159 @@
+function initPage() {
+    initLineChart();
+}
+
+//Initialize line chart Skeleton
+function initLineChart() {
+    var options = {
+        series: [],
+        chart: {
+            type: "line",
+            height: 500,
+            id: "lineChart", //selector name
+            group: "charts",
+            toolbar: {
+                show: true,
+                tools: {
+                    download: false,
+                    selection: false,
+                    zoom: "<div></div>",
+                    zoomin: false,
+                    zoomout: false,
+                    pan: false,
+                    reset: '<button class="btn btn-outline-light">Reset Zoom</button>',
+                },
+            },
+            zoom: {
+                enabled: true,
+                resetIcon: {
+                    offsetX: 1000,
+                    offsetY: 0,
+                    fillColor: "#FF9900",
+                    strokeColor: "#FF9900",
+                },
+                selection: {
+                    background: "#FF9900",
+                    border: "#FF9900",
+                },
+            },
+        },
+        //Pre-loading title while fetching price data
+        title: {
+            text: "Loading Charts...",
+            align: "center",
+            style: {
+                color: "#f0f0f0",
+                fontFamily: "Source Code Pro, monospace",
+            },
+        },
+        //Data Labels are labels with value on the chart
+        dataLabels: {
+            enabled: false,
+        },
+        //Defining chart color
+        colors: ["#FF9900"],
+        //Line Stroke
+        stroke: {
+            show: true,
+            curve: "smooth",
+            lineCap: "butt",
+            width: 4,
+            dashArray: 0,
+        },
+        xaxis: {
+            type: "datetime",
+            labels: {
+                style: {
+                    colors: "#f0f0f0",
+                    fontSize: "1rem",
+                    fontFamily: "Source Code Pro, monospace",
+                },
+            },
+            //vertical crosshairs styling
+            crosshairs: {
+                show: true,
+                width: 1,
+                stroke: {
+                    colors: "#f0f0f0",
+                    width: 2,
+                    dashArray: 0,
+                },
+            },
+            //x-axis tooltip
+            tooltip: {
+                enabled: false,
+            },
+        },
+        //Words to show then chart is loading
+        noData: {
+            text: "Loading Charts...",
+            align: "center",
+            verticalAlign: "middle",
+            style: {
+                color: "#FF9900",
+                fontSize: "3em",
+                fontFamily: "Tourney, cursive",
+            },
+        },
+    };
+
+    let lineChart = new ApexCharts(document.querySelector("#lineChart"), options);
+    lineChart.render();
+}
+
+//Line Chart Renderer
+function renderLineChart() {
+    //Get line data
+    processedData = bitcoinSupplyData();
+
+    //Start date of data and end date
+    startDate = new Date(processedData[0][0]);
+    startDate = startDate.getUTCDate() + "/" + (startDate.getUTCMonth() + 1) + "/" + startDate.getUTCFullYear();
+    endDate = new Date(processedData[processedData.length - 1][0]);
+    endDate = endDate.getUTCDate() + "/" + (endDate.getUTCMonth() + 1) + "/" + endDate.getUTCFullYear();
+
+    //Update chart data
+    ApexCharts.exec("lineChart", "updateSeries", [{ data: processedData }]);
+
+    ApexCharts.exec(
+        "lineChart",
+        "updateOptions",
+        {
+            title: {
+                text: `Bitcoin Supply Chart (From ${startDate} To ${endDate})`,
+            },
+            tooltip: {
+                custom: function ({ series, seriesIndex, dataPointIndex, w }) {
+                    priceString = Number(series[seriesIndex][dataPointIndex].toFixed(0));
+                    dateString = new Date(w.globals.initialSeries[0].data[dataPointIndex][0]);
+                    dateString = dateString.getUTCDate() + " " + dateString.toLocaleString("default", { month: "short" }) + " " + dateString.getUTCFullYear();
+                    return `
+                    <div id="tooltip">
+                    <div id="tooltip-top">${dateString}</div>
+                    <div id="tooltip-bottom">Supply: ${priceString.toLocaleString()}</div>
+                    <div id="tooltip-bottom">% Mined: ${(priceString / 210000).toFixed(2)}%</div>
+                    </div>`;
+                },
+            },
+            yaxis: {
+                type: "numeric",
+                tickAmount: 10,
+                labels: {
+                    style: {
+                        colors: "#f0f0f0",
+                        fontSize: "1rem",
+                        fontFamily: "Source Code Pro, monospace",
+                    },
+                    formatter: function (val) {
+                        return Number(val.toFixed(0)).toLocaleString();
+                    },
+                    snap: true,
+                },
+            },
+        },
+        true,
+        true
+    );
+}
+
+function renderChartAnnotations() {}
